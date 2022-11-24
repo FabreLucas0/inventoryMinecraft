a9723625169dbb797d5c505e6723ca4f2d5fe9db
imuny <92868641+imuny0@users.noreply.github.com>
2022-11-24 16:43:11 +0100

    parse json CA MARCHE

--- inventoryMinecraft/src/main/java/org/example/Main.java
+++ inventoryMinecraft/src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
+
+///home/lucas/moi/an2/qualite_de_dev/projet/inventoryMinecraft/inventoryMinecraft/src/main/resources/test.json
 package org.example;
 
-import com.google.gson.JsonArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.io.FileReader;
 public class Main {
     public static void main(String[] args) {
-        JsonArray arr;
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("/home/lucas/moi/an2/qualite_de_dev/projet/inventoryMinecraft/inventoryMinecraft/src/main/resources/test.json"));
+            JSONObject jsonObject = (JSONObject)obj;
+            String name = (String)jsonObject.get("nom");
+            System.out.println("Name: " + name);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
