78d73a37e8184173599312a6fbf7ca7de7541f70
AntLafI <91462362+AntLafI@users.noreply.github.com>
2022-12-15 23:03:36 +0100

    changement style programme

--- IUT CRAFT/demo/src/main/java/com/example/demo/IutCraftController.java
+++ IUT CRAFT/demo/src/main/java/com/example/demo/IutCraftController.java
@@ -12,24 +12,20 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 
-public class IutCraftController{
+public class IutCraftController {
     @FXML
-    private Label erreur;
+    private Label error;
 
     @FXML
-    private Label blockName;
-
-    @FXML
-    private TextField toto;
+    private TextField txt;
 
     @FXML
     private CheckBox alpha;
@@ -44,16 +40,21 @@ public class IutCraftController{
     private CheckBox Shapeless;
 
     @FXML
-    private CheckBox wiki;
+    private ListView<String> ingredientsList;
+
+    @FXML
+    private Label pattern;
+
+    @FXML
+    private Label blockName;
 
     @FXML
     private ListView<String> list_craft;
 
     @FXML
-    private ListView<String> ingredientsList;
+    private Button bwiki;
 
-    @FXML
-    private Label pattern;
+
 
     public void unCheckAlph(){
         alpha.setSelected(false);
@@ -71,55 +72,60 @@ public class IutCraftController{
         Shapeless.setSelected(false);
     }
 
-    public void clearListView() {
-        ListView<String> items = (ListView<String>) list_craft.getItems();
-        items.refresh();
-    }
 
     @FXML
-    protected void onHelloButtonClick() throws IOException, ParseException {
-        // Lire la valeur du champ de texte
-        Object obj = new JSONParser().parse(new FileReader(new File("src/main/resources/merged_recipes.json")));
+    protected void ClickButton() throws Exception {
+
+        // parsing JSON of all the recipes
+        Object obj = new JSONParser().parse(new FileReader(new File("src/main/resources/com/example/demo/merged_recipes.json")));
         JSONObject jo = (JSONObject) obj;
 
-        String text = toto.getText();
-        erreur.setText("recherche non trouvée");
-        //Scene recipes = new Scene(fxmlLoader.load(), 710, 600);
-
+        // setting up variables
+        String text = txt.getText().replaceAll(" ", "_");
+        error.setText("recherche non trouvée");
         ArrayList<String> names = new ArrayList<>();
         ArrayList<String> researchNames = new ArrayList<>();
+        Recipies lshaped = new Shaped_recipes(new Bloc());
+        Recipies shapeless = new Shapeless_recipes(new Bloc());
+
 
         for (int i = 0; i < jo.size(); i++) {
             names.add(jo.keySet().toArray()[i].toString());
+
         }
+
 
         researchNames.clear();
-        for (String element : names) {
-            element = element.replaceAll("_", " ");
-            if (element.contains(text)) {
-                researchNames.add(element);
-            }
-            if (element.equals(text)) {
-                if (wiki.selectedProperty().getValue()) {
-                    Recherche find = new Recherche("https://minecraft.fandom.com/wiki/" + element);
-                    find.start();
-                }
+
+        // selecting the corresponding elements to search into depending on the selected filter
+        if(Shaped.selectedProperty().getValue() == true) {
+
+            element(text, researchNames, lshaped);
+        }
+        else if(Shapeless.selectedProperty().getValue() == true) {
+            element(text, researchNames, shapeless);
+        }
+        else {
+            for (String element : names) {
+
+                element(text, researchNames, element);
             }
         }
 
-        // Algorithme de tri à bulles d'une liste
+
+        // bubble sort for researchNames
         boolean trie = false;
         while (!trie) {
             trie = true;
             for (int i = 0; i < researchNames.size() - 1; i++) {
-                // par ordre alphabétique
+                // alphabetical order
                 if (researchNames.get(i).compareTo(researchNames.get(i + 1)) > 0 && alpha.isSelected()) {
                     String temp = researchNames.get(i);
                     researchNames.set(i, researchNames.get(i + 1));
                     researchNames.set(i + 1, temp);
                     trie = false;
                 }
-                // par ordre alphabétique inverse
+                // reversed alphabetical order
                 else if (researchNames.get(i + 1).compareTo(researchNames.get(i)) > 0 && reverse.isSelected()) {
                     String temp = researchNames.get(i + 1);
                     researchNames.set(i + 1, researchNames.get(i));
@@ -128,42 +134,79 @@ public class IutCraftController{
                 }
             }
         }
+
+        // if nothing is found
         if (researchNames.isEmpty()) {
-            erreur.setVisible(true);
+            // displaying an error message and clearing the research
+            bwiki.setVisible(false);
+            error.setVisible(true);
             list_craft.getItems().clear();
         }
-        else if (researchNames.contains(toto.getText())) {
-            erreur.setVisible(false);
-            blockName = new Label(toto.getText());
+
+        // when a block is found, opens a new page
+        else if (researchNames.contains(txt.getText())) {
+            error.setVisible(false);
+            blockName = new Label(txt.getText());
             ingredientsList = new ListView<>();
-            FXMLLoader fxmlLoader = new FXMLLoader(new File("src/main/resources/RecipesViewer.fxml").toURL());
+
+            //creating the new page
+            FXMLLoader fxmlLoader = new FXMLLoader(new File("src/main/resources/com/example/demo/RecipesViewer.fxml").toURL());
             GridPane root = fxmlLoader.load();
             Scene scene = new Scene(root, 710, 600);
             Stage recipe = new Stage();
-            HashMap<String, Object> item = new HashMap<>((HashMap<String, Object>)jo.get(toto.getText().replaceAll(" ", "_")));
+            recipe.setTitle("Votre Craft");
+            scene.getStylesheets().add(String.valueOf(this.getClass().getResource("style.css")));
+
+            // getting the necessary data for displaying
+            HashMap<String, Object> item = new HashMap<>((HashMap<String, Object>)jo.get(txt.getText().replaceAll(" ", "_")));
+            // for shapeless recipes
             if(item.get("type").equals("crafting_shapeless")){
+
+                // getting the ingredients
                 ArrayList<Object> ingredient = (ArrayList<Object>) item.get("ingredients");
                 ObservableList<String> ingredientsShapeless = FXCollections.observableArrayList();
                 for (int i=0; i < ingredient.size(); i++){
                     ingredientsShapeless.add(((HashMap) ingredient.get(i)).get("item").toString().replaceAll("minecraft:", "").replaceAll("_", " "));
                 }
+
+                //placing everything on the scene (on the window)
                 ingredientsList.setItems(ingredientsShapeless);
                 root.add(ingredientsList, 1, 0);
                 blockName.setText(blockName.getText() + " (Shapeless)");
             }
+            // for shaped recipes
             else{
-
+                // Creating a label for the recipe
                 Label pattern = new Label("");
-                for (String line: (ArrayList<String>)item.get("pattern")) {
-                    pattern.setText(pattern.getText() + line + "\n");
-                }
+                ArrayList<String> blockPattern = (ArrayList<String>)item.get("pattern");
 
+                // getting the 3x3 recipe
+                int j = 0;
+                for (int i = 0; i < blockPattern.size(); i++) {
+                    j = 3 - blockPattern.get(i).length();
+                    pattern.setText(pattern.getText() + blockPattern.get(i) + " ".repeat(j) + "\n");
+                    if(blockPattern.size() < 3){
+                        blockPattern.add("   ");
+                    }
+                }
+                // recplacing empty spaces with a random character to keep the 3x3 format
+                pattern.setText(pattern.getText().replaceAll(" ", "▢"));
+
+                // getting ingredients
                 HashMap<String, Object> ingredient = (HashMap<String, Object>) item.get("key");
                 ObservableList<String> temp = FXCollections.observableArrayList(ingredient.keySet());
                 for(int i=0; i < temp.size(); i++){
-                    HashMap<String, Object> itemData = (HashMap<String, Object>) ingredient.get(temp.get(i));
-                    temp.set(i, temp.get(i) + " = " + itemData.get("item").toString().replaceAll("minecraft:","").replaceAll("_", " "));
+                    // if there is multiple kind of the same block
+                    if((ingredient.get(temp.get(i)) instanceof ArrayList<?>)){
+                        temp.set(i, temp.get(i) + " = any " + ((HashMap<String,Object>)((ArrayList<Object>) ingredient.get(temp.get(i))).get(0)).get("item").toString().replaceAll("minecraft:","").replaceAll("_", " "));
+                    }
+                    else {
+                        HashMap<String, Object> itemData = (HashMap<String, Object>) ingredient.get(temp.get(i));
+                        temp.set(i, temp.get(i) + " = " + itemData.get("item").toString().replaceAll("minecraft:", "").replaceAll("_", " "));
+                    }
                 }
+
+                //placing everything on the scene (on the window)
                 ingredientsList.setItems(temp);
                 root.add(ingredientsList, 1,1);
                 root.add(pattern, 0, 1);
@@ -171,20 +214,51 @@ public class IutCraftController{
                 pattern.setPadding(new Insets(0,0,0,70));
                 blockName.setText(blockName.getText() + " (Shaped)");
             }
+
+            // placing the block's name and launching the scene
             blockName.setFont(new Font(17));
             root.add(blockName, 0, 0);
             recipe.setScene(scene);
             recipe.showAndWait();
-
         }
+        // when nothing was found
         else {
-            System.out.println(toto.getText());
-            erreur.setVisible(false);
+            bwiki.setVisible(false);
+            error.setVisible(false);
+
+            // clear previous researches and display new ones
             list_craft.getItems().clear();
             list_craft.getItems().addAll(researchNames);
+
+            // when clicking in an element in the list, it automatically fills the search bar
             list_craft.setOnMouseClicked(event -> {
-                toto.setText(list_craft.getSelectionModel().getSelectedItem());
+                txt.setText(list_craft.getSelectionModel().getSelectedItem());
             });
+        }
+
+        System.out.println();
+    }
+
+    // fill the researchNames list with found blocks from the research
+    private void element(String text, ArrayList<String> researchNames, String element) {
+        if (element.contains(text)) {
+            researchNames.add(element.replaceAll("_", " "));
+        }
+        // display wiki button
+        if (element.equals(text)) {
+            bwiki.setVisible(true);
+            bwiki.setOnAction(event -> {
+                Recherche find = new Recherche("https://minecraft.fandom.com/wiki/" + element);
+                find.start();
+            });
+        }
+    }
+
+    // changing the researches to only "lshaped" type (shaped or shapeless)
+    private void element(String text, ArrayList<String> researchNames, Recipies lshaped) throws Exception {
+        for (String element : lshaped.getNom()) {
+
+            element(text, researchNames, element);
         }
     }
 }
